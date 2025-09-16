8b24908360e548baa179c2cd7ab128a9b7530e61
Danilo <d.torquato.dev@gmail.com>
2025-09-16 19:52:14 -0300

    Inicializando o projeto.

---
+++ src/main/java/com/java/ecommerce/basketservice/controller/ProductController.java
@@ -0,0 +1,30 @@
+package com.java.ecommerce.basketservice.controller;
+
+import com.java.ecommerce.basketservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<Void> getAllProducts(){
+
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Void> getAllProductById(@PathVariable Long id){
+
+        return ResponseEntity.ok().build();
+    }
+}
+
---
+++ src/main/java/com/java/ecommerce/basketservice/service/ProductService.java
@@ -0,0 +1,14 @@
+package com.java.ecommerce.basketservice.service;
+
+import org.springframework.stereotype.Service;
+@Service
+public class ProductService {
+
+
+    public void getAllCategories () {
+
+    }
+    public void  getCategoriesById() {
+
+    }
+}

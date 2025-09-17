f0c18484bff8291dada813066bda4b9b915f8cbf
Danilo <d.torquato.dev@gmail.com>
2025-09-17 18:23:53 -0300

    feat:Adiciona o docker-compose, e confiura e configura o cache  para busca de produtos

--- src/main/java/com/java/ecommerce/basketservice/service/ProductService.java
+++ src/main/java/com/java/ecommerce/basketservice/service/ProductService.java
@@ -3,20 +3,28 @@ package com.java.ecommerce.basketservice.service;
 import com.java.ecommerce.basketservice.client.PlatziStoreClient;
 import com.java.ecommerce.basketservice.client.response.PlatziProductResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ProductService {
 
     private final PlatziStoreClient platziStoreClient;
 
+    @Cacheable(value = "products")
     public List<PlatziProductResponse> getAllProducts() {
+        log.info("Getting all products");
         return platziStoreClient.getAllProducts();
     }
-    public PlatziProductResponse  getProductById(Long id) {
-return platziStoreClient.getProductbById(id);
+
+    @Cacheable(value = "product", key = "#productId")
+    public PlatziProductResponse  getProductById(Long productId) {
+        log.info("Getting product with id: {}", productId);
+        return platziStoreClient.getProductbById(productId);
     }
 }
